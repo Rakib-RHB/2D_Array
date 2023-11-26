412a134dbfe8a909898bc67edaa1365595fef936
Rakib Hasan <140387583+Rakib-RHB@users.noreply.github.com>
2023-11-26 22:46:45 +0600

    Add files via upload

---
+++ 2d array declaration.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int m[3][4],i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            scanf("%d",&m[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            printf("%d ",m[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+}
